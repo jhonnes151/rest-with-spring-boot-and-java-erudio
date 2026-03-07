7fa9886f38f86bd75271ffcca4be512bff269eea
Jhonnes <jhonnes151@gmail.com>
2026-03-07 08:55:49 -0300

    Atualizaçao DTO

---
+++ src/main/java/br/com/erudio/data/dto/PersonDTO.java
@@ -0,0 +1,70 @@
+package br.com.erudio.data.dto;
+
+
+import java.io.Serializable;
+import java.util.Objects;
+
+
+public class PersonDTO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String gender;
+
+    public PersonDTO() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof PersonDTO person)) return false;
+        return Objects.equals(getId(), person.getId()) && Objects.equals(getFirstName(), person.getFirstName()) && Objects.equals(getLastName(), person.getLastName()) && Objects.equals(getAddress(), person.getAddress()) && Objects.equals(getGender(), person.getGender());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getFirstName(), getLastName(), getAddress(), getGender());
+    }
+}
---
+++ src/main/java/br/com/erudio/exception/ExceptionResponse.java
@@ -0,0 +1,4 @@
+package br.com.erudio.exception;
+
+public record ExceptionResponse(java.util.Date timestamp, String message, String details) {
+}
